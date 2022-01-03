be5c2a07d508ced8702417652b4c6e76530291b6
taishan1994 <461600371@qq.com>
2022-01-03 16:55:13 +0800

    commit

---
+++ config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+data_path = "./data"
+model_path = data_path + "/model/"
+train_data_path = data_path + "/sentiment_analysis_trainingset.csv"
+validate_data_path = data_path + "/sentiment_analysis_validationset.csv"
+test_data_path = data_path + "/sentiment_analysis_testa.csv"
+test_data_predict_output_path = data_path + "/testa_predict.csv"
